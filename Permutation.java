ac4e3bcd3033014d3c4fb0df2b85bb2ef1e46804
Sumith Amin N <72722741+sumith208@users.noreply.github.com>
2023-11-22 14:56:19 +0530

    Update Permutation.java
    changed the method name from permute to permutation

--- Permutation.java
+++ Permutation.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Permutation{
-
-    public static void permute(String s,String d){
+//Permutation
+    public static void permutation(String s,String d){
         if(s.length()==0){
             System.out.println(d);
             return;
@@ -13,13 +13,13 @@ public class Permutation{
             String lsub= s.substring(0, i);
             String rsub= s.substring(i+1);
             String w=lsub+rsub;
-            permute(w,d+c);
+            permutation(w,d+c);
         }
     }
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter Permutation String : ");
         String s= sc.next();
-        permute(s, "");
+        permutation(s, "");
     }
 }
